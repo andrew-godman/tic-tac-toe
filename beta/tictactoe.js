7806cc645ea34bad263aa306be4e33f2bd1fc0d8
Andrew Godman <andrew.godman@me.com>
2018-10-02 22:53:07 +1000

    changed some logic on beta files, commiting for a snapshot for tonights work

--- beta/tictactoe.js
+++ beta/tictactoe.js
@@ -1,51 +1,76 @@
-// ### Technical Requirements
-//
-// Your app must:
-//
-// * **Render a game in the browser**
-// * **Switch turns** between more than one player
-// * **Design logic for winning** & **visually display which player won**
-// * **Include separate HTML / CSS / JavaScript files**
-// * Stick with **KISS (Keep It Simple Stupid)** principles
-// * Use **Javascript** for **DOM manipulation**
-// * **Deploy your game online**, where the rest of the world can access it
-// * Use **semantic markup** for HTML and CSS (adhere to best practices)
-// 
-// * **today** - At least one artefact of either pseudocode, flow diagram, wireframe, mockup or storyboard demonstrating planning - **Due today** 
-// * A **working game, built by you**, hosted somewhere on the internet
-// * A **link to your hosted working game** in the URL section of your GitHub repo
-// * A **git repository hosted on GitHub**, with a link to your hosted game, and **frequent commits** dating back to the very beginning of the project
-// * **A ``readme.md`` file** with explanations of the technologies used, the approach taken, installation instructions, unsolved problems, etc.
-//
-// ### Suggested Ways to Get Started
-//
-// * **Break the project down into different components** (data, presentation, markup, style, DOM manipulation) and brainstorm each component individually. Use whiteboards!
-// * **Use your Development Tools** (console.log, inspector, debugger, etc) to debug and solve problems
-// * Work through the lessons in class & ask questions when you need to! Think about adding relevant code to your game each night, instead of, you know... _procrastinating_.
-// * **Commit early, commit often.** Don’t be afraid to break something because you can always go back in time to a previous version.
-// * **Consult documentation resources** (MDN etc.) at home to better understand what you’ll be getting into.
-// * **Don’t be afraid to write code that you know you will have to remove later.** Create temporary elements (buttons, links, etc) that trigger events if real data is not available. For example, if you’re trying to figure out how to change some text when the game is over but you haven’t solved the win/lose game logic, you can create a button to simulate that until then.
-
-// get mouse click of the 9 clicked items
-// display where use as clicked on (or cpu)
-// if 3 in a row display win
-
 var box = document.querySelector('.board')
-
-
 
 box.addEventListener('click', boxClicked)
 
+var board = new Array(9).fill("")
+var win = "";
 function boxClicked() {
-    columnClicked = event.target.className;
-    rowClicked = event.target.parentElement.className;
-    
-    console.log(columnClicked, rowClicked);
-     
-    
-}
 
-// event.target
-// <div class=​"column2">​2​</div>​
-// event.target.parentElement
-// <div class=​"row1">​…​</div>​
+    columnClicked = event.target;
+    columnClicked.style.backgroundColor = "white";
+    player1 = "andrew"
+    player2 = "CPU"
+
+    var columnClickedNumber = (columnClicked.className.charAt(4)) -1
+    board.splice(columnClickedNumber, 1, player1,);
+    
+    if( board[0] === player1 && board[1] === player1 && board[2] === player1 ||
+        board[3] === player1 && board[4] === player1 && board[5] === player1 ||
+        board[6] === player1 && board[7] === player1 && board[8] === player1 ||
+        board[0] === player1 && board[3] === player1 && board[6] === player1 ||
+        board[1] === player1 && board[4] === player1 && board[7] === player1 ||
+        board[2] === player1 && board[5] === player1 && board[8] === player1 ||
+        board[0] === player1 && board[4] === player1 && board[8] === player1 ||
+        board[2] === player1 && board[4] === player1 && board[6] === player1 )
+        {
+            var win = player1;
+            console.log("you win");
+        }
+    
+    if( board[0] === player2 && board[1] === player2 && board[2] === player2 ||
+        board[3] === player2 && board[4] === player2 && board[5] === player2 ||
+        board[6] === player2 && board[7] === player2 && board[8] === player2 ||
+        board[0] === player2 && board[3] === player2 && board[6] === player2 ||
+        board[1] === player2 && board[4] === player2 && board[7] === player2 ||
+        board[2] === player2 && board[5] === player2 && board[8] === player2 ||
+        board[0] === player2 && board[4] === player2 && board[8] === player2 ||
+        board[2] === player2 && board[4] === player2 && board[6] === player2 )
+        {
+            var win = player2;
+            console.log("you win");
+        }
+
+    }
+
+// if(board[0] === player1 && board[1] === player1 && board[2] === player1) {
+//     var win = player1;
+//     console.log("you win");
+// }
+// if(board[3] === player1 && board[4] === player1 && board[5] === player1) {
+//     var win = player1;
+//     console.log("you win");
+// }
+// if(board[6] === player1 && board[7] === player1 && board[8] === player1) {
+//     var win = player1;
+//     console.log("you win");
+// }
+// if(board[0] === player1 && board[3] === player1 && board[6] === player1) {
+//     var win = player1;
+//     console.log("you win");
+// }
+// if(board[1] === player1 && board[4] === player1 && board[7] === player1) {
+//     var win = player1;
+//     console.log("you win");
+// }
+// if(board[2] === player1 && board[5] === player1 && board[8] === player1) {
+//     var win = player1;
+//     console.log("you win");
+// }
+// if(board[0] === player1 && board[4] === player1 && board[8] === player1) {
+//     var win = player1;
+//     console.log("you win");
+// }
+// if(board[2] === player1 && board[4] === player1 && board[6] === player1) {
+//     var win = player1;
+//     console.log("you win");
+ 
