9d93859f3ea2e5fcb4a72c12b2325b122fb67b37
Andrew Godman <andrew.godman@me.com>
2018-10-03 11:48:50 +1000

    added working turn logic

--- beta/tictactoe.js
+++ beta/tictactoe.js
@@ -1,18 +1,41 @@
-var box = document.querySelector('.board')
-
-box.addEventListener('click', boxClicked)
-
-var board = new Array(9).fill("")
+var box = document.querySelector('.board');
+var player1 = "";
+var player2 = "";
+var board = new Array(9).fill("");
 var win = "";
+var turn = "";
+var submitButon = document.querySelector("#submit");
+
+box.addEventListener('click', boxClicked);
+
+submitButon.addEventListener('click', funtionSubmiterthing);
+
+function funtionSubmiterthing() {
+    event.preventDefault()
+    player1 = document.getElementById("Player1").value;
+    player2 = document.getElementById("Player2").value;
+    turn = player1;
+    document.querySelector(".Turn").innerText = player1 + "'s" + " turn"
+}
+
 function boxClicked() {
 
     columnClicked = event.target;
     columnClicked.style.backgroundColor = "white";
-    player1 = "andrew"
-    player2 = "CPU"
-
+    columnClicked.textContent = turn;
+    
+    // document.querySelector(".Winner").innerHTML = "Player 1"
     var columnClickedNumber = (columnClicked.className.charAt(4)) -1
-    board.splice(columnClickedNumber, 1, player1,);
+    board.splice(columnClickedNumber, 1, turn,);
+    
+    if  (turn === player1) {
+            turn = player2
+    }
+    else {
+            turn = player1
+    }
+
+    document.querySelector(".Turn").innerText = turn + "'s" + " turn"
     
     if( board[0] === player1 && board[1] === player1 && board[2] === player1 ||
         board[3] === player1 && board[4] === player1 && board[5] === player1 ||
@@ -24,7 +47,10 @@ function boxClicked() {
         board[2] === player1 && board[4] === player1 && board[6] === player1 )
         {
             var win = player1;
-            console.log("you win");
+            document.querySelector(".Turn").innerText = ""
+            document.querySelector(".Winner").innerText = win + " Wins!"
+            box.removeEventListener('click', boxClicked);
+
         }
     
     if( board[0] === player2 && board[1] === player2 && board[2] === player2 ||
@@ -37,40 +63,10 @@ function boxClicked() {
         board[2] === player2 && board[4] === player2 && board[6] === player2 )
         {
             var win = player2;
-            console.log("you win");
+            document.querySelector(".Turn").innerText = ""
+            document.querySelector(".Winner").innerText = win + " Wins!"
+            box.removeEventListener('click', boxClicked);
         }
+        
 
     }
-
-// if(board[0] === player1 && board[1] === player1 && board[2] === player1) {
-//     var win = player1;
-//     console.log("you win");
-// }
-// if(board[3] === player1 && board[4] === player1 && board[5] === player1) {
-//     var win = player1;
-//     console.log("you win");
-// }
-// if(board[6] === player1 && board[7] === player1 && board[8] === player1) {
-//     var win = player1;
-//     console.log("you win");
-// }
-// if(board[0] === player1 && board[3] === player1 && board[6] === player1) {
-//     var win = player1;
-//     console.log("you win");
-// }
-// if(board[1] === player1 && board[4] === player1 && board[7] === player1) {
-//     var win = player1;
-//     console.log("you win");
-// }
-// if(board[2] === player1 && board[5] === player1 && board[8] === player1) {
-//     var win = player1;
-//     console.log("you win");
-// }
-// if(board[0] === player1 && board[4] === player1 && board[8] === player1) {
-//     var win = player1;
-//     console.log("you win");
-// }
-// if(board[2] === player1 && board[4] === player1 && board[6] === player1) {
-//     var win = player1;
-//     console.log("you win");
- 
